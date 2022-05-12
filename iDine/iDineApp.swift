c582fd9e6c0b64c7c9b3667801c1225ec9636b79
Nirajan Shrestha <nirajan.shrestha@ebpearls.com>
2022-05-12 14:27:06 +0545

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by ebpearls on 5/12/22.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
